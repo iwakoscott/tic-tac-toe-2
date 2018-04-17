00ec0174a324809075ce4e04d254fca0c82cbf06
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-04-17 11:52:24 -0700

    commit before branching off of master to work on StartMenu comp

--- src/components/StartMenu/StartMenu.js
+++ src/components/StartMenu/StartMenu.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './StartMenu.css';
 
 export default () => (
-  <div className="container">
+  <div className="container viewport-centered">
     <h1 className="display-4">Tic-Tac-Toe</h1>
+    <h2 className="mt-5">Start</h2>
   </div>
 );
